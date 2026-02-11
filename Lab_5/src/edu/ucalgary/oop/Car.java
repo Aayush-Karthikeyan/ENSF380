8bf65b100d2c0a56c6149043cf8eea8f04a29570
Mazin <shmazin3@gmail.com>
2026-02-11 11:48:26 -0700

    Car.java

--- Lab_5/src/edu/ucalgary/oop/Car.java
+++ Lab_5/src/edu/ucalgary/oop/Car.java
@@ -1,5 +1,60 @@
-package edu.ucalgary.oop;
+public class Car {
+    // Atributes
+    private String model;
+    private Driver driver;
+    private Engine engine;
 
-public class car {
-    
+    // Primary constructor
+    public Car(String model, Driver driver, Engine engine) {
+        this.model = model;
+
+        // Deep copy
+        this.driver = new Driver(driver);
+        this.engine = new Engine(engine);
+    }
+
+    // Copy method
+    public Car copy(boolean deepCopy) {
+
+        if (deepCopy) {
+            return new Car(
+                this.model,
+                new Driver(this.driver),
+                new Engine(this.engine)
+            );
+        } 
+        else {
+            // shallow copy
+            return new Car(
+                this.model,
+                this.driver,
+                this.engine
+            );
+        }
+    }
+    // Getters
+    public String getModel() {
+        return model;
+    }
+
+    public Driver getDriver() {
+        return driver;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    // Setters
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setDriver(Driver driver) {
+        this.driver = new Driver(driver); // keep deep copy safety
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = new Engine(engine); // keep deep copy safety
+    }
 }
