f6e37ebe279aef600dc47b73c07e05bd43d33b57
Aayush-Karthikeyan <aayush.karthikeyan@ucalgary.ca>
2026-02-11 11:50:25 -0700

    Engine.java

--- Lab_5/src/edu/ucalgary/oop/Engine.java
+++ Lab_5/src/edu/ucalgary/oop/Engine.java
@@ -1,3 +1,25 @@
+package edu.ucalgary.oop;
+
 public class Engine {
-    
+    private String engineType;
+
+    public Engine(String engineType) {
+        this.engineType = engineType;
+    }
+
+    // Deep copy constructor
+    public Engine(Engine other) {
+        if (other == null) {
+            throw new IllegalArgumentException("other Engine cannot be null");
+        }
+        this.engineType = other.engineType;
+    }
+
+    public String getEngineType() {
+        return engineType;
+    }
+
+    public void setEngineType(String engineType) {
+        this.engineType = engineType;
+    }
 }
